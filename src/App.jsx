74039ef53161df27a73b5bfd8263396fc6a47df2
Semilore Idowu <semijacks27@gmail.com>
2022-01-14 14:51:11 +0100

    created first mutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
-import { useQuery } from 'react-query';
+import { useMutation } from 'react-query';
 import Post from './Post';
 import client from './react-query-client';
 
 export const fetcher = (url) => fetch(url).then((res) => res.json());
+const timer = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+      console.log(' i was run');
+    }, duration);
+  });
+};
 
 function App() {
-  const [postId, setPostId] = useState(null);
+  const mutation = useMutation(() => timer(1000));
 
-  const { isLoading, data: posts } = useQuery(
-    'posts',
-    () => fetcher(`https://jsonplaceholder.typicode.com/posts`),
-    {
-      select: (posts) => posts.slice(0, 5),
-    }
-  );
-
-  if (postId !== null) {
-    return <Post postId={postId} goBack={() => setPostId(null)} />;
-  }
-
-  function mutateTitle(id) {
-    client.setQueryData(['post', id], (oldData) => {
-      if (oldData) {
-        return {
-          ...oldData,
-          title: 'boom boom boom mutated',
-        };
-      }
-    });
+  async function callMutation() {
+    console.log('about to call mutation');
+    await mutation.mutateAsync();
+    console.log('mutation called');
   }
 
   return (
     <div className='App'>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        posts.map((post) => {
-          const cachedPost = client.getQueryData(['post', post.id]);
-
-          return (
-            <p key={post.id}>
-              <a onClick={() => setPostId(post.id)} href='#'>
-                {post.id} - {post.title}
-              </a>
-              {cachedPost ? '(visited)' : ''}
-              <button onClick={() => mutateTitle(post.id)}>Mutate Title</button>
-            </p>
-          );
-        })
-      )}
+      <h1>Mutations</h1>
+      <button onClick={callMutation}>Submit</button>
     </div>
   );
 }
